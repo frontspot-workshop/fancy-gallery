a95382dc42ef5504e312f2c3fea8051eae42ee76
Denis <d.telyuk@gmail.com>
2020-10-07 00:16:52 +0600

    app Init; pixabay instead of unsplash; material-ui pagination & tags toggle;

--- src/App.js
+++ src/App.js
@@ -2,68 +2,46 @@ import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { ACCESS_KEY, API_URL } from "./credentials";
 import "./App.css";
+import "fontsource-roboto";
+import Button from "@material-ui/core/Button";
+import { Pagination, ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
+import { Box } from "@material-ui/core";
+
+
 
 function App() {
   const [images, setImages] = useState([]);
-  const [page, setPages] = useState(1);
-  const [query, setQuery] = useState();
-  const loader = useRef(null);
-
-  const fetchImages = () => {
-    axios
-      .get(
-        `${API_URL}/photos/?page=${page}&per_page=20&client_id=${ACCESS_KEY}`
-      )
-      .then((response) => setImages([...images, ...response.data]));
-  };
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [imagesPerPage] = useState(20);
+  const [query, setQuery] = useState("");
 
   const searchImages = () => {
     axios
       .get(
-        `${API_URL}/search/photos/?query=${query}&page=${page}&per_page=20`,
-        {
-          headers: { Authorization: `Client-ID ${ACCESS_KEY}` },
-        }
+        `${API_URL}?key=${ACCESS_KEY}&q=${query}&page=${page}&per_page=${imagesPerPage}&image_type=photo`
       )
-      .then((response) => setImages([...images, ...response.data.results]));
+      .then((response) => {
+        setImages(response.data.hits);
+        setTotalPages(Math.ceil(response.data.totalHits / imagesPerPage));
+      });
   };
 
-  const handleClick = (newQuery) => {
+  const handleTagClick = (event, newQuery) => {
     if (newQuery !== query) {
       setImages([]);
-      setPages(1);
+      setPage(1);
     }
     setQuery(newQuery);
   };
 
-  const handleObserver = (entities) => {
-    const target = entities[0];
-
-    if (target.intersectionRatio > 0) {
-      setPages((page) => page + 1);
-    }
+  const handleChangePage = (event, value) => {
+    setPage(value);
   };
 
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "0px",
-      treshold: 1,
-    };
-
-    const observer = new IntersectionObserver(handleObserver, options);
-
-    if (loader.current) {
-      observer.observe(loader.current);
-    }
-  }, []);
 
   useEffect(() => {
-    if (query) {
-      searchImages();
-    } else {
-      fetchImages();
-    }
+    searchImages();
   }, [page, query]);
 
   return (
@@ -71,28 +49,63 @@ function App() {
       <header className="header">
         <h1>Fancy Gallery</h1>
       </header>
+
       <div className="tags">
-        <button onClick={() => handleClick("cats")}>Cats</button>
-        <button onClick={() => handleClick("dogs")}>Dogs</button>
-        <button onClick={() => handleClick("coffee")}>Coffee</button>
-        <button onClick={() => handleClick("react")}>React</button>
-        <button onClick={() => handleClick()}>Random</button>
+        <ToggleButtonGroup
+          value={query}
+          exclusive
+          onChange={handleTagClick}
+          aria-label="Image tags"
+        >
+          <ToggleButton value="Cats" aria-label="Cats images">
+            Cats
+          </ToggleButton>
+          <ToggleButton value="Dogs" aria-label="Dogs images">
+            Dogs
+          </ToggleButton>
+          <ToggleButton value="Coffee" aria-label="Coffee images">
+            Coffee
+          </ToggleButton>
+          <ToggleButton value="React" aria-label="React">
+            React
+          </ToggleButton>
+          <ToggleButton value="" aria-label="Random">
+            Random
+          </ToggleButton>
+        </ToggleButtonGroup>
       </div>
+
+      <Box
+        display="flex"
+        justifyContent="center"
+        m={1}
+        p={1}
+        bgcolor="background.paper"
+      >
+        <Pagination
+          className="pages"
+          count={totalPages}
+          showFirstButton
+          showLastButton
+          page={page}
+          variant="outlined"
+          onChange={handleChangePage}
+        />
+      </Box>
       <div className="image-grid">
         {images.map((image) => {
-          const { id, color, urls, alt_description } = image;
+          const { id, webformatURL, tags } = image;
           return (
             <div
               className="image-item"
               key={id}
-              style={{ backgroundColor: color }}
+              style={{ backgroundColor: "grey" }}
             >
-              <img src={urls.small} alt={alt_description} />
+              <img src={webformatURL} alt={tags} />
             </div>
           );
         })}
       </div>
-      <div ref={loader}>Loading...</div>
     </div>
   );
 }
