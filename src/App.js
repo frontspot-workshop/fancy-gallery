0e3ae98bc154f86630e05f5bd87ca37a88c38034
Denis <d.telyuk@gmail.com>
2020-10-07 10:50:32 +0600

    workshop 1 tasks completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ACCESS_KEY, API_URL } from "./credentials";
+import { Pagination, ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
+import { AppBar, Badge, Grid, makeStyles, Paper, Toolbar, Typography } from "@material-ui/core";
 import "./App.css";
 import "fontsource-roboto";
-import Button from "@material-ui/core/Button";
-import { Pagination, ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
-import { Box } from "@material-ui/core";
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  offset: theme.mixins.toolbar,
+  title: {
+    flexGrow: 1,
+  },
+  tags: {
+    background:"white"
+  }
+}));
 
 function App() {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [totalImages, setTotalImages] = useState(0);
   const [imagesPerPage] = useState(20);
   const [query, setQuery] = useState("");
+  const [tags] = useState(["Cats", "Dogs", "Coffee", "React", ""]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    searchImages();
+  }, [page, query, imagesPerPage, totalImages]);
 
   const searchImages = () => {
     axios
@@ -23,6 +45,7 @@ function App() {
       )
       .then((response) => {
         setImages(response.data.hits);
+        setTotalImages(response.data.totalHits);
         setTotalPages(Math.ceil(response.data.totalHits / imagesPerPage));
       });
   };
@@ -39,73 +62,78 @@ function App() {
     setPage(value);
   };
 
-
-  useEffect(() => {
-    searchImages();
-  }, [page, query]);
-
   return (
-    <div className="container">
-      <header className="header">
-        <h1>Fancy Gallery</h1>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Fancy Gallery
+          </Typography>
+          <ToggleButtonGroup
+            className={classes.tags}
+            value={query}
+            exclusive
+            onChange={handleTagClick}
+            aria-label="Image tags"
+          >
+            {tags.map((tag) => {
+              return (
+                <ToggleButton value={tag} aria-label={{ tag } + " images"}>
+                  {tag === "" ? "Random" : tag}
+                </ToggleButton>
+              );
+            })}
+          </ToggleButtonGroup>
+        </Toolbar>
+      </AppBar>
 
-      <div className="tags">
-        <ToggleButtonGroup
-          value={query}
-          exclusive
-          onChange={handleTagClick}
-          aria-label="Image tags"
-        >
-          <ToggleButton value="Cats" aria-label="Cats images">
-            Cats
-          </ToggleButton>
-          <ToggleButton value="Dogs" aria-label="Dogs images">
-            Dogs
-          </ToggleButton>
-          <ToggleButton value="Coffee" aria-label="Coffee images">
-            Coffee
-          </ToggleButton>
-          <ToggleButton value="React" aria-label="React">
-            React
-          </ToggleButton>
-          <ToggleButton value="" aria-label="Random">
-            Random
-          </ToggleButton>
-        </ToggleButtonGroup>
-      </div>
+      <div className={classes.offset} />
 
-      <Box
-        display="flex"
-        justifyContent="center"
-        m={1}
-        p={1}
-        bgcolor="background.paper"
-      >
-        <Pagination
-          className="pages"
-          count={totalPages}
-          showFirstButton
-          showLastButton
-          page={page}
-          variant="outlined"
-          onChange={handleChangePage}
-        />
-      </Box>
-      <div className="image-grid">
-        {images.map((image) => {
-          const { id, webformatURL, tags } = image;
-          return (
-            <div
-              className="image-item"
-              key={id}
-              style={{ backgroundColor: "grey" }}
-            >
-              <img src={webformatURL} alt={tags} />
-            </div>
-          );
-        })}
-      </div>
+      <Grid container>
+        <Grid item xs={12}>
+          <Paper className="image-grid">
+            {images.map((image) => {
+              const { id, webformatURL, tags } = image;
+              return (
+                <div
+                  className="image-item"
+                  key={id}
+                  style={{ backgroundColor: "grey" }}
+                >
+                  <img src={webformatURL} alt={tags} />
+                </div>
+              );
+            })}
+          </Paper>
+        </Grid>
+        <Grid item xs={12} container justify="center">
+          <Paper className={classes.paper} elevation={0}>
+            <Badge color="secondary" badgeContent={totalImages} max={999}>
+              {query === "" && (
+                <Pagination
+                  className="pages"
+                  count={totalPages}
+                  showFirstButton
+                  showLastButton
+                  page={page}
+                  variant="outlined"
+                  onChange={handleChangePage}
+                />
+              )}
+              {query !== "" && (
+                <Pagination
+                  className="pages"
+                  count={totalPages}
+                  showFirstButton
+                  page={page}
+                  variant="outlined"
+                  onChange={handleChangePage}
+                />
+              )}
+            </Badge>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
