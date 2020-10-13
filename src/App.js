c1d6467aaf95df935caded36257b54001d562644
alexander_koryagin <alexander_koryagin@epam.com>
2020-10-13 18:56:03 +0600

    fetch and search images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
+import {ACCESS_KEY, API_URL} from './credentials';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [images, setImages] = useState([]);
+    const [page, setPage] = useState(1);
+    const loader = useRef(null);
+    const [query, setQuery] = useState('')
+
+
+    const fetchImages = () => {
+        axios.get(`${API_URL}/photos/?client_id=${ACCESS_KEY}&per_page=40&page=${page}`)
+            .then(res => setImages([...images, ...res.data]));
+    };
+
+    const searchImages = () => {
+        axios.get(`${API_URL}/search/photos/?client_id=${ACCESS_KEY}&per_page=40&page=${page}&query=${query}`)
+            .then(res => setImages([...images, ...res.data.results]));
+    };
+
+    const handleObserver = entities => {
+        const target = entities[0];
+
+        if (target.intersectionRatio > 0) {
+            setPage(page => page + 1)
+        }
+    };
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 1.0,
+        }
+        const observer = new IntersectionObserver(handleObserver, options);
+        if (loader.current) {
+            observer.observe(loader.current);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (query) {
+            searchImages();
+        } else {
+            fetchImages();
+        }
+    }, [page, query]);
+
+    const handleClick =  (newQuery) => {
+        if(newQuery !== query){
+            setImages([]);
+            setPage(1);
+        }
+        setQuery(newQuery);
+        setQuery(newQuery);
+    }
+
+    return (
+        <div className="container">
+            <header className="header">
+                <h1>Fancy Gallery</h1>
+            </header>
+            <div className="tags">
+                <button onClick={()=>handleClick('cats')}>Cats</button>
+                <button onClick={()=>handleClick('dogs')}>Dogs</button>
+                <button onClick={()=>handleClick('react')}>React</button>
+                <button onClick={()=>handleClick('')}>Random</button>
+            </div>
+            <div className="image-grid">
+                {images.map(image => {
+                    const {id, alt_description, urls, color} = image;
+                    return (
+                        <div className="image-item" key={id} style={{backgroundColor: color}}>
+                            <img src={urls.small} alt={alt_description}/>
+                        </div>)
+                })}
+            </div>
+            <div ref={loader}>Loading...</div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/credentials.js
@@ -0,0 +1,2 @@
+export const ACCESS_KEY='****************************************';
+export const API_URL='https://api.unsplash.com'
